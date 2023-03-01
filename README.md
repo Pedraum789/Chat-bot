f6cb54563539e3e07bc1601017190d3b76994fb3
Pedro Sanchez <pedro.sanchez@infoprice.co>
2023-02-28 23:46:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chat-bot
+
+## Arquivo objeto.json
+### Aqui identificamos o que gostariamos que o chat bot respondesse e a identificação dele, sendo:
+- patterns -> Identificação da pergunta
+- responses -> Resposta para a pergunta
+
+## Vídeo explicativo:
+https://www.youtube.com/watch?v=1rjYcVoFGz0
+
+## Imagem de demonstração:
+![image](https://user-images.githubusercontent.com/35739467/221931656-52d8613f-dfef-4112-8775-0cda4de6daf5.png)
---
+++ main.py
@@ -0,0 +1,26 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+myChatBot.loadModel()
+
+#criar o modelo
+#myChatBot.createModel()
+
+
+
+
+print("Bem vindo ao Chatbot")
+
+
+pergunta = input("como posso te ajudar?")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("posso lhe ajudar com algo a mais?")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+    opcao_resposta = myChatBot.chatbot_response_opcao(intencao)
+
+print("Foi um prazer atendê-lo")
